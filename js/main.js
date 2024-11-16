1e84c0958a9f32c578e7d83bdf898c26dfb48002
Hiếu Nguyễn <ngvannhieu2005@gmail.com>
2024-11-17 00:49:12 +0700

    layout web - v1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Sticky scrolled
+window.onscroll = function () {
+  const header = document.querySelector('header');
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+};
